d383e35e4e9b81ca1db920e34b44f0483d548405
Dmitry Ilyin <dilyin@mirantis.com>
2014-10-13 18:07:21 +0400

    Fix if

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,32 +1,33 @@
-class { 'corosync':
-  enable_secauth    => false,
-  bind_address      => '127.0.0.0',
-  multicast_address => '239.1.1.2',
-}
-
-corosync::service { 'pacemaker':
-  version => '0',
-}
-
-cs_property { 'stonith-enabled' :
-  value   => 'false',
-}
-
-cs_property { 'no-quorum-policy' :
-  value   => 'ignore',
-}
-
-Service['corosync'] ->
-Cs_property<||> ->
-Cs_primitive<||>
-
 include demo_service
-
 $ha = true
 
 if $ha {
+
+  class { 'corosync':
+    enable_secauth    => false,
+    bind_address      => '127.0.0.0',
+    multicast_address => '239.1.1.2',
+  }
+
+  corosync::service { 'pacemaker':
+    version => '0',
+  }
+
+  cs_property { 'stonith-enabled' :
+    value   => 'false',
+  }
+
+  cs_property { 'no-quorum-policy' :
+    value   => 'ignore',
+  }
+
+  Service['corosync'] ->
+  Cs_property<||> ->
+  Cs_primitive<||>
+
   # individual inherit method
   # include demo_service_inherit
   # ha override definition method
   service_ha_override { 'nginx' :}
+
 }
